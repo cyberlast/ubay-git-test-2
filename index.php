144539cc880ebd2cbdd8ee8a13fe9fc095f5fd69
cyberlast <cyberlast155@gmail.com>
2023-11-22 06:02:17 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quisquam et magnam expedita aspernatur sunt suscipit animi autem earum officiis exercitationem nam impedit explicabo, dignissimos fuga ducimus! Aliquam, ex consectetur?</p>
+    <ul>
+        <li>list</li>
+    </ul>
 </body>
 </html>
